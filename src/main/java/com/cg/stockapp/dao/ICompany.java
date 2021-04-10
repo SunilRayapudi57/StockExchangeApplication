167f602efe32a7c39196d8c3d33b1acedb9b5fa1
Surendra babu Bommidi <surendrababu1999@gmail.com>
2021-04-10 15:37:33 +0530

    Update ICompany.java

--- src/main/java/com/cg/stockapp/dao/ICompany.java
+++ src/main/java/com/cg/stockapp/dao/ICompany.java
@@ -5,5 +5,8 @@ import com.cg.stockapp.dto.Company;
 public interface ICompany {
 	
 	public boolean addCompany(Company company);
-	
+	public List<Company> getAllCompanyInfo();
+	public Company getCompanyDetails(String companyId);
+ 	public boolean  updateCompanyInfo(Company info);
+	public boolean deleteCompanyInfo(String companyId);
 }
